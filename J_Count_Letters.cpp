7ef33d72c0d37fc82edddf9102d7dea6ca31e770
Brilliant-Developer1 <email2obail@gmail.com>
2023-06-08 17:30:19 +0600

    first commit

---
+++ J_Count_Letters.cpp
@@ -0,0 +1,90 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int frq[26] = {0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        for (char j = 'a'; j <= 'z'; j++)
+        {
+            if (s[i] == j)
+            {
+                frq[s[i] - 'a']++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        char a = i + 'a';
+        if (frq[i])
+        {
+            cout << a << " : " << frq[i] << endl;
+        }
+    }
+
+    return 0;
+}
